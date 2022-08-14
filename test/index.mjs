51cdf7e78a389c009f425d80b823423181e6684a
Tahlil <bsse0803@iit.du.ac.bd>
2022-08-14 15:25:59 +0600

    Create accounts

--- test/index.mjs
+++ test/index.mjs
@@ -1,3 +1,45 @@
 import { loadStdlib, ask } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+import * as backend from '../build/index.main.mjs';
 const stdlib = loadStdlib();
+
+const isAlice = await ask.ask(
+    `Are you Alice?`,
+    ask.yesno
+  );
+  const who = isAlice ? 'Alice' : 'Bob';
+
+let acc = null;
+const createAcc = await ask.ask(
+  `Would you like to create an account on devnet`,
+  ask.yesno
+);
+if (createAcc) {
+  acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+} else {
+  const secret = await ask.ask(
+    `What is your account secret?`,
+    (x => x)
+  );
+  acc = await stdlib.newAccountFromSecret(secret);
+}
+
+let ctc = null;
+if (isAlice) {
+  ctc = acc.contract(backend);
+  ctc.getInfo().then((info) => {
+    console.log(`The contract is deployed as = ${JSON.stringify(info)}`); });
+} else {
+  const info = await ask.ask(
+    `Please paste the contract information:`,
+    JSON.parse
+  );
+  ctc = acc.contract(backend, info);
+}
+
+const fmt = (x) => stdlib.formatCurrency(x, 5);
+const getBalance = async () => fmt(await stdlib.balanceOf(acc));
+
+const before = await getBalance();
+console.log(`Your balance is ${before}`);
+
+const interact = { };
